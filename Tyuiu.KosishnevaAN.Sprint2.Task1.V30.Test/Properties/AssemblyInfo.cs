d31fcdff54c2b62b85a0ddc43baaf05611a431df
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-10-10 23:46:59 +0500

    Task1

---
+++ Tyuiu.KosishnevaAN.Sprint2.Task1.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosishnevaAN.Sprint2.Task1.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosishnevaAN.Sprint2.Task1.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7e39faa-e93e-4a71-84ac-6e6c97c421d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KosishnevaAN.Sprint2.Task1.V30/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosishnevaAN.Sprint2.Task1.V30.Lib;
+
+namespace Tyuiu.KosishnevaAN.Sprint2.Task1.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 657;
+            int b = 874;
+            int c = 14;
+            int d = 654;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #1 | Выполнила: Косишнева А. Н. | ИСТНб-23-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #2                                                             *");
+            Console.WriteLine("* Тема: Логические операции                                             *");
+            Console.WriteLine("* Задание #1                                                            *");
+            Console.WriteLine("* Вариант #30                                                           *");
+            Console.WriteLine("* Выполнила: Косишнева Анастасия Николаенвна | ИСТНб-23-1               *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,       *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в       *");
+            Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !, ^,                 *");
+            Console.WriteLine("* последовательность операций не должна нарушаться), а также            *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность*");
+            Console.WriteLine("* (массив): (True, False, True, True, True, False),                     *");
+            Console.WriteLine("* при a = 657, b = 874, c = 14, d = 654                                 *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine(" a = " + a);
+            Console.WriteLine(" b = " + b);
+            Console.WriteLine(" a = " + c);
+            Console.WriteLine(" b = " + d);
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
