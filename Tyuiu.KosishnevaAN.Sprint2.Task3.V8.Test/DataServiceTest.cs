ff467b754c50b6c76a726d3042c14e082270f539
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-10-11 00:43:52 +0500

    Task3

---
+++ Tyuiu.KosishnevaAN.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KosishnevaAN.Sprint2.Task3.V8.Lib;
+
+namespace Tyuiu.KosishnevaAN.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = -4.588;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = 0.25;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double res = ds.Calculate(x);
+            double wait = -219.75;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+
