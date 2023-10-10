b2c0e23be374c66101501577d29a81dd8fcd11c1
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-10-11 00:53:00 +0500

    Task4

---
+++ Tyuiu.KosishnevaAN.Sprint2.Task4.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosishnevaAN.Sprint2.Task4.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosishnevaAN.Sprint2.Task4.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1915b3f-3029-4d6c-969b-fb3e4376f744")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
