5829a77dba638a6ca437d16a9a7c65c0809de50a
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-10-13 00:53:27 +0500

    Task6

---
+++ Tyuiu.KosishnevaAN.Sprint2.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KosishnevaAN.Sprint2.Task6.V10.Lib;
+
+namespace Tyuiu.KosishnevaAN.Sprint2.Task6.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("4.10.2023", ds.FindDateOfPreviousDay(2023,10,5));
+        }
+    }
+}
---
+++ Tyuiu.KosishnevaAN.Sprint2.Task6.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosishnevaAN.Sprint2.Task6.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosishnevaAN.Sprint2.Task6.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a968f910-16b3-4409-b324-6d36f95cd533")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
